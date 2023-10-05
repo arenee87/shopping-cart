dc91ed40a1ee0b7d4aecb5bae2aa642b6d1805d3
Alece Kemp <alece.kemp@gmail.com>
2023-10-05 12:27:05 -0500

    created shopping cart app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
+function App() {
+  
+
+  return (
+    
+      <Container>Hello Peace</Container>
+    
+  )
+}
+
+export default App
